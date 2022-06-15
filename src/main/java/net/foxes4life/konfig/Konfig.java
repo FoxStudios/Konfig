44fc61d01096c0b56b49450016ccd8b8d0efffcf
Flux <flustixshinu@gmail.com>
2022-06-15 21:08:35 +0200

    loading stuff part 1

--- src/main/java/net/foxes4life/konfig/Konfig.java
+++ src/main/java/net/foxes4life/konfig/Konfig.java
@@ -1,8 +1,15 @@
 package net.foxes4life.konfig;
 
+import com.google.gson.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Konfig {
     String configPath;
     String configID;
+
+    JsonObject jsonData;
 
     public Konfig(String id) {
         configID = id;
@@ -15,6 +22,15 @@ public class Konfig {
     }
 
     public void load() {
+        try {
+            jsonData = JsonParser.parseString(Files.readString(Path.of(configPath))).getAsJsonObject();
+        } catch (Exception ex) {
+            System.out.println("No config file found for id " + configID + "! Creating new config...");
+            jsonData = new JsonObject();
+        }
+    }
+
+    public void save() {
 
     }
 }
